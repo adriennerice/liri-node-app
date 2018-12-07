36c631f05a35d191e30cdc01a9496e43e750ecec
Adrienne Rice <adrienne.rice@gmail.com>
2018-12-06 22:38:57 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # liri-node-app
 LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+How to Use LIRI
+
+1. In the Terminal type node liri.js [choose a command] [search term]
+2. Type one of the following commands and associated search term:
+    
+    * node liri.js concert-this (artist name)
+    * node liri.js spotify-this-song (song title)
+    * node liri.js movie-this (movie title)
+    * do-what-it-says
+
+Walkthrough
+
+1. Search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+
+    * Name of the venue
+    * Venue location
+    * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+    ![Image of Band Search](/images/concertSearch.png)
+    
+    ![Image of Movie Search blank](/images/concertSearchBlank.png)
+
+2. You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+
+    * Artist(s)
+    * The song's name
+    * A preview link of the song from Spotify
+    * The album that the song is from
+
+    ![Image of Song Search](/images/songSearch.png)
+
+3. Search for a movie and retrieve data from the OMDB API. If a movie isn't typed in, the program will output data for the movie 'Mr. Nobody.'
+
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.
+
+   ![Image of Movie Search](/images/movieSearch.png)
+
+   ![Image of Movie Search blank](/images/movieSearchBlank.png)
+
+4. LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands. It will run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+
+![Image of Random text file content](/images/random.png)
--- liri.js
+++ liri.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-//var inquirer = require("inquirer");
-var axios = require('axios'); //Bands in Town, Spotify and OMDB APIs
+var axios = require('axios'); //Used to call Bands in Town, Spotify and OMDB APIs
 var fs = require('fs');
 var moment = require('moment');
 var dotenv = require('dotenv');
@@ -10,36 +9,9 @@ const { g, b, gr, r, y } =  require('./console');
 
 var spotify = new Spotify(keys.spotify);
 
-
 let command = process.argv[2];
-let search = process.argv[3];
-
-// inquirer.prompt([
-  
-//     {
-//       type: "list",
-//       name: "command",
-//       message: "Select type of search:",
-//       choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
-//     },
-  
-//     {
-//         type: "input",
-//         name: "search",
-//         message: "Enter artist, song or movie to search.",
-        
-//     }
-    
-  
-//   ]).then(function(response) {
-
-//     // console.log(response.command);
-
-//     remoteControl(response.command, response.search);
-   
-//   });
-
-
+let search =  process.argv.slice(3).join(" ");
+//process.argv[3];
 
 
 function remoteControl(command, search){
@@ -78,13 +50,12 @@ function concert(search){
             console.log(b("**** Artist: " + search + " ****\n"));
             
             for(var i = 0; i < response.data.length; i++){
-                // Then we print out the Name of the venue, Venue location, Date of the Event (use moment to format this as "MM/DD/YYYY")
+                // Print out the Name of the venue, Venue location, Date of the Event (use moment to format this as "MM/DD/YYYY")
                 console.log(g("Venue: ") + response.data[i].venue.name);
                 console.log(g("Location: ") + response.data[i].venue.city + ", " + response.data[i].venue.region );
                 console.log(g("Date: ") + moment(response.data[i].datetime).format("MM/DD/YYYY") );//2017-03-19T11:00:00'
                 console.log(r("----------------------------"));
             }
-
     })
     .catch(function(err) {
         console.log(err);
@@ -95,7 +66,6 @@ function spotifySearch(search){
     spotify.search({ type: 'track', query: search, limit: 20 }).then(function(response) {
         for(var i = 0; i < response.tracks.items.length; i++){
         // Then we print out the Artist(s), The song's name, A preview link of the song from Spotify, The album that the song is from
-            // Update response.data items 
             console.log(g("Song Name: ") + response.tracks.items[i].name);
             for(var j = 0; j < response.tracks.items[i].artists.length; j++){
                 console.log(g("Artist(s): ") + response.tracks.items[i].artists[j].name);
@@ -111,16 +81,6 @@ function spotifySearch(search){
     })
 };
 
-// function spotifyNull (){
-//     spotify
-//     .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
-//     .then(function(data) {
-//         console.log(data); 
-//     })
-//     .catch(function(err) {
-//         console.error('Error occurred: ' + err); 
-//     });
-// }
 
 function movieSearch(search){
     axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(function(response) {
@@ -152,7 +112,6 @@ function random(){
           return console.log(error);
         }
 
-      
         // Then split it by commas (to make it more readable)
         var dataArr = data.split(","); // spotify-this-song, "I Want It That Way" => [spotify-this-song, "I Want It That Way"]
       
